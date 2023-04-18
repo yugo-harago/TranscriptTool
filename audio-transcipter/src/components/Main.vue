8e2b46c07cc5dc3ae430ffdb4cd9f4b0b05819d3
Yugohg <yugoharago@gmail.com>
2023-04-19 01:20:42 +0900

    Added text

--- audio-transcipter/src/components/Main.vue
+++ audio-transcipter/src/components/Main.vue
@@ -1,16 +1,15 @@
 
 
 <template>
-  <!-- <div class="p-2 mb-1 bg-light">
+  <div class=" mb-1 bg-light fixed-top">
       <div class="container-fluid d-xl-flex align-items-xl-center py-1" style="height: 205px;">
-          <div style="height: 92px;background: #ffe39a;width: 100%;"></div>
+        <AudioPlayer :audioUrl="audioUrl"></AudioPlayer>
       </div>
-  </div> -->
-  <AudioPlayer :audioUrl="audioUrl"></AudioPlayer>
+  </div>
   <section>
-      <div class="container">
+      <div class="container" style="margin-top: 205px;">
           <div class="col">
-              <p><span style="color: rgb(102, 102, 102);">「戦争が終ったら、こんどはまた急に何々主義だの、何々主義だの、あさましく騒ぎまわって、演説なんかしているけれども、私は何一つ信用できない気持です。主義も、思想も、へったくれも要らない。男は嘘をつく事をやめて、女は慾を捨てたら、それでもう日本の新しい建設が出来ると思う。」</span><br><span style="color: rgb(102, 102, 102);">　私は焼け出されて津軽の生家の居候になり、鬱々として楽しまず、ひょっこり訪ねて来た小学時代の同級生でいまはこの町の名誉職の人に向って、そのような八つ当りの愚論を吐いた。名誉職は笑って、</span><br><span style="color: rgb(102, 102, 102);">「いや、ごもっとも。しかし、それは、逆じゃありませんか。男が慾を捨て、女が嘘をつく事をやめる、とこう来なくてはいけません。」といやにはっきり反対する。</span><br><span style="color: rgb(102, 102, 102);">　私はたじろぎ、</span><br><br></p>
+              <Text />
           </div>
       </div>
   </section>
@@ -18,11 +17,13 @@
 
 <script>
 import AudioPlayer from './AudioPlayer.vue';
-import audioFile from '../assets/rd480.mp3';
+import Text from './Text.vue'
+import audioFile from '../assets/DazaiOsamu_Uso.mp3';
 
 export default {
   components: {
     AudioPlayer,
+    Text
   },
   data() {
     return {
